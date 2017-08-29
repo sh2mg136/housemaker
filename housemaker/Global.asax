8d40179846b6cc06b4c360782c4577c4303b43b0
sh2mg136 <sh2mg136@gmail.com>
2017-08-29 09:01:08 +0300

    Добавьте файлы проекта.

---
+++ housemaker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="housemaker.MvcApplication" Language="C#" %>
---
+++ housemaker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(housemaker.Startup))]
+namespace housemaker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
