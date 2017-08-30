ccc8d0b232961c387111ba8e21c49fd67169d47f
sh2mg136 <sh2mg136@gmail.com>
2017-08-30 09:24:19 +0300

    init commit

--- housemaker/Controllers/HomeController.cs
+++ housemaker/Controllers/HomeController.cs
@@ -13,6 +13,13 @@ namespace housemaker.Controllers
             return View();
         }
 
+        public ActionResult Album()
+        {
+            ViewBag.Message = "Фотоальбом";
+
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ housemaker/Models/CarouselItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace housemaker.Models
+{
+
+    public class CarouselItem
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsShownCarousel { get; set; }
+        public byte[] Photo { get; set; }
+        public string Url { get; set; }
+        public string Base64Image { get; set; }
+        public string Headline { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public string ButtonText { get; set; }
+        public string ButtonUrl { get; set; }
+    }
+
+}
